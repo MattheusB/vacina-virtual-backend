b3804bfd098698305724fbef6a013078d1e39f0b
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2019-10-09 23:04:29 +0200

    criacao do cartao de vacina

---
+++ api/cartao-vacina/cartao.model.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var cartaoSchema = new Schema({
+    sus:{
+        type:String,
+        required:true
+    },
+
+    cpf:{
+        type:String,
+        required:true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        unique: true
+      },
+    
+    nascimento: {
+        type: Date,
+        required: true,
+
+    },
+
+    vacina: {
+        type: Array,
+        required: false,
+    }
+    
+
+    
+})
